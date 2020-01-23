84550e25e7986739fa0f33eb59febe4d0376862a
Milan Petrík <milpet.petrik@gmail.com>
2020-01-23 09:13:41 +0100

    alpha0.3

--- Prezencka/Prezencka/Holidays.xaml.cs
+++ Prezencka/Prezencka/Holidays.xaml.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using System.IO;
 
 namespace Prezencka
 {
@@ -15,6 +18,31 @@ namespace Prezencka
         public Holidays()
         {
             InitializeComponent();
+
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+
+            //Add a page to the document
+            PdfPage page = document.Pages.Add();
+
+            //Create PDF graphics for the page
+            PdfGraphics graphics = page.Graphics;
+
+            //Set the standard font
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+
+            //Draw the text
+            graphics.DrawString("Hello World!!!", font, PdfBrushes.Black, new PointF(0, 0));
+
+            //Save the document to the stream
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream);
+
+            //Close the document
+            document.Close(true);
+
+            //Save the stream as a file in the device and invoke it for viewing
+            Xamarin.Forms.DependencyService.Get<ISave>().SaveAndView("Output.pdf", "application / pdf", stream);
         }
     }
 }
